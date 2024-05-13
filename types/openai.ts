56b243a62d48cb7baf965832178a58a7df7fd171
Joshua B <desertflame3@gmail.com>
2024-05-13 15:36:14 -0400

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -5,10 +5,7 @@ export interface OpenAIModel {
   tokenLimit: number;
 }
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo-0125',
-  GPT_3_5_16K = 'gpt-3.5-turbo-16k',
-  GPT_3_5_AZ = 'gpt-35-turbo',
-  GPT_3_5_16K_AZ = 'gpt-35-turbo-16k',
+  GPT_3_5 = 'gpt-3.5-turbo',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4o',
   GPT_4_128K = 'gpt-4-turbo',
@@ -18,25 +15,7 @@ export const fallbackModelID = OpenAIModelID.GPT_3_5;
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
     id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
-    maxLength: 48000,
-    tokenLimit: 16000,
-  },
-  [OpenAIModelID.GPT_3_5_16K]: {
-    id: OpenAIModelID.GPT_3_5_16K,
-    name: 'GPT-3.5-16K',
-    maxLength: 48000,
-    tokenLimit: 16000,
-  },
-  [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
-  },
-  [OpenAIModelID.GPT_3_5_16K_AZ]: {
-    id: OpenAIModelID.GPT_3_5_16K_AZ,
-    name: 'GPT-3.5-16K',
+    name: 'GPT-3.5-turbo',
     maxLength: 48000,
     tokenLimit: 16000,
   },
