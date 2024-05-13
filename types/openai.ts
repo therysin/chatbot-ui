af493f2ed1f905e42011c8cf1cf9e616f512e7fc
Joshua B <desertflame3@gmail.com>
2024-05-13 15:33:37 -0400

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -10,8 +10,8 @@ export enum OpenAIModelID {
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_3_5_16K_AZ = 'gpt-35-turbo-16k',
   GPT_4 = 'gpt-4',
-  GPT_4_32K = 'gpt-4-32k',
-  GPT_4_128K = 'gpt-4-0125-preview',
+  GPT_4_32K = 'gpt-4o',
+  GPT_4_128K = 'gpt-4-turbo',
 }
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
 export const fallbackModelID = OpenAIModelID.GPT_3_5;
@@ -48,13 +48,13 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   },
   [OpenAIModelID.GPT_4_32K]: {
     id: OpenAIModelID.GPT_4_32K,
-    name: 'GPT-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32000,
+    name: 'GPT-4o',
+    maxLength: 384000,
+    tokenLimit: 128000,
   },
   [OpenAIModelID.GPT_4_128K]: {
     id: OpenAIModelID.GPT_4_128K,
-    name: 'GPT-4-128K',
+    name: 'GPT-4-turbo',
     maxLength: 384000,
     tokenLimit: 128000,
   },
